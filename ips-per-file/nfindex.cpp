7554f1a5f590176a22402a99798858f18c7e5642
Gerard Wagener <gerard.wagener@circl.lu>
2014-09-11 10:52:14 +0200

    Count IPs and put them in a map

--- ips-per-file/nfindex.cpp
+++ ips-per-file/nfindex.cpp
@@ -1,9 +1,6 @@
 /*
- * Issues:
- *
- * The index contains more addresses than there are in the nfcapd file?
- * Bug in libnfdump, index, indexing code?
- * TODO write C program that just dumps the addresses and compare
+ * 70MB of memory are consumed.
+ * Serialized files has: 14MB
  */
 #include <libnfdump/libnfdump.h>
 #include <map>
@@ -32,24 +29,42 @@ int main (int argc, char* argv[])
 {
     libnfstates_t* states;
     master_record_t* rec;
-
+    uint64_t num_v6_flows;
+    char c;
     if (argc != 2) {
         fprintf(stderr,"An nfcapd file needs to be passed as command line argument\n");
         return (EXIT_FAILURE);
     }
+
+    /* Index how many times an IP address emerged in a file */
+    map < uint32_t, uint32_t > index;
     
     /* Initialize libnfdump */
     states = initlib(NULL, argv[1],NULL);
- 
     if (states) {
         do {
             rec = get_next_record(states);
             if (rec) {
+                if ( (rec->flags & FLAG_IPV6_ADDR ) != 0 ) {
+                    //FIXME Cannot handle v6 flows but count them
+                    num_v6_flows++;
+                } else {
+                    index[rec->v4.dstaddr]++;
+                    index[rec->v4.srcaddr]++;
+                }
             }
         } while (rec);
         
         /* Close the nfcapd file and free up internal states */
         libcleanup(states);
     }
+    // Serialize the data
+    ofstream idxFile;
+    idxFile.open("test.dat", ofstream::binary);
+    boost::archive::text_oarchive oarch(idxFile);
+    oarch<<index;
+    idxFile.close();
+    cout << "Press a key to terminate "<<endl;
+    cin >> c;
     return(EXIT_SUCCESS);
 } 
